62956b40c9b72bb2d9fdd2a1efd08694ff750254
David Flerlage <david@David-Flerlages-MacBook-Pro.local>
2009-12-19 15:19:10 -0500

    removed generics

--- framework/src/org/celluloidlang/core/EveryFunction.java
+++ framework/src/org/celluloidlang/core/EveryFunction.java
@@ -3,13 +3,13 @@ package org.celluloidlang.core;
 import org.celluloidlang.constraints.defined.Input;
 import org.celluloidlang.reactive.ReactiveNumber;
 
-public class EveryFunction<T extends Input> {
+public class EveryFunction {
 	
 	protected ReactiveNumber everyTime;
-	protected T input;
-	protected T[] evals;
+	protected Input input;
+	protected Input[] evals;
 	
-	public EveryFunction(ReactiveNumber everyTime, T eventObject, T ... evals) {
+	public EveryFunction(ReactiveNumber everyTime, Input eventObject, Input ... evals) {
 		this.everyTime = everyTime;
 		this.input = eventObject;
 		this.evals = evals;
