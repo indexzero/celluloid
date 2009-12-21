5369a3291bb7828a68e20c82675791fc3e05e504
scmcduffee <sean.mcduffee@gmail.com>
2009-12-21 10:46:18 -0500

    every works (for a basic case anyway)

--- parser/STGrammarRulesTestCase.java
+++ parser/STGrammarRulesTestCase.java
@@ -36,9 +36,10 @@ public class STGrammarRulesTestCase extends TestCase {
     public void runAllTests() throws IOException, RecognitionException {
         //this.shouldThrowOnUndefinedInput();
         //this.shouldThrowOnUndefinedFunction();
-        this.shouldGenerateHelloCh1();
+       // this.shouldGenerateHelloCh1();
         //this.shouldGenerateHelloCh2();
         //this.shouldGenerateHelloCh3();
+        this.shouldGenerateHelloCh4();
         //this.shouldGenerateHelloEvents();
         //this.shouldGenerateHelloWorld();
         //this.shouldGenerateEventDefinition();
@@ -92,6 +93,21 @@ public class STGrammarRulesTestCase extends TestCase {
 
     public void shouldGenerateHelloCh3() throws IOException, RecognitionException {
         STTestRunner testRunner = new STTestRunner(this.testPath + "helloCh3.cld", this.templatePath) {
+            public CommonTree getTree(celluloidParser parser) throws RecognitionException {
+                return (CommonTree)parser.program().getTree();
+            }
+
+            public StringTemplate getTemplate(celluloidWalker walker) throws RecognitionException {
+                celluloidWalker.program_return r = walker.program();
+                return (StringTemplate)r.getTemplate();
+            }
+        };
+
+        testRunner.RunTest();
+    }
+
+    public void shouldGenerateHelloCh4() throws IOException, RecognitionException {
+        STTestRunner testRunner = new STTestRunner(this.testPath + "helloCh4.cld", this.templatePath) {
             public CommonTree getTree(celluloidParser parser) throws RecognitionException {
                 return (CommonTree)parser.program().getTree();
             }
